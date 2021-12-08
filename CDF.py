5460c6ac652fde5983f36085cd9e2d3e723d03d6
Daniel Gecášek <daniel.gecasek@tuke.sk>
2021-12-08 16:59:19 +0100

    refactored with DRY principle in mind, code cleanup.

--- CDF.py
+++ CDF.py
@@ -18,136 +18,58 @@ def writeUncompressed():
 	start = tm.time()
 	settings['Compressed'] = 0
 	cdf_file = cdflib.cdfwrite.CDF(GLOBAL_FN, cdf_spec=settings,delete=True)
-	#####
 	globalAttrs={}
-	globalAttrs['extern_field']={0: gData["extern_field"]}
-	globalAttrs['datetime']={0: gData["datetime"]}
-	globalAttrs['arr_len']={0: gData["arr_len"]}
-	globalAttrs['lcr']={0: gData["lcr"]}
-	globalAttrs['ucr']={0: gData["ucr"]}
-	globalAttrs['ecr']={0: gData["ecr"]}
-	globalAttrs['geo_lat']={0: gData["geo_lat"]}
-	globalAttrs['geo_lon']={0: gData["geo_lon"]}
-	globalAttrs['geo_rad']={0: gData["geo_rad"]}
-	globalAttrs['loc_lat']={0: gData["loc_lat"]}
-	globalAttrs['loc_lon']={0: gData["loc_lon"]}
-	globalAttrs['starting_rig']={0: gData["starting_rig"]}
-	globalAttrs['rig_step']={0: gData["rig_step"]}
-	globalAttrs['step_limit']={0: gData["step_limit"]}
-	#######
+	for attrData in gData:
+		globalAttrs[attrData] = {0: gData[attrData]}
 	cdf_file.write_globalattrs(globalAttrs)
 	varinfo = {}
-	varinfo["Variable"] = "rig"
 	varinfo["Data_Type"] = cdflib.cdfwrite.CDF.CDF_REAL8 #'CDF_FLOAT'
 	varinfo["Num_Elements"] = 1
 	varinfo["Rec_Vary"] = 0
 	varinfo["Dim_Sizes"] = [sz]
 	varinfo["Compress"] = 0
-	cdf_file.write_var(varinfo, var_data=aData["rig"])
-	varinfo["Variable"] = "v"
-	cdf_file.write_var(varinfo, var_data=aData["v"] )
-	varinfo["Variable"] = "rad"
-	cdf_file.write_var(varinfo, var_data=aData["rad"] )
-	varinfo["Variable"] = "eth"
-	cdf_file.write_var(varinfo, var_data=aData["eth"])
-	varinfo["Variable"] = "efi"
-	cdf_file.write_var(varinfo, var_data=aData["efi"])
-	varinfo["Variable"] = "ath"
-	cdf_file.write_var(varinfo, var_data=aData["ath"])
-	varinfo["Variable"] = "afi"
-	cdf_file.write_var(varinfo, var_data=aData["afi"])
-	varinfo["Variable"] = "time"
-	cdf_file.write_var(varinfo, var_data=aData["time"])
-	varinfo["Variable"] = "length"
-	cdf_file.write_var(varinfo, var_data=aData["length"])
+	for attrData in aData:
+		varinfo["Variable"] = attrData
+		cdf_file.write_var(varinfo, var_data=aData[attrData])
+	
 	cdf_file.close()
 	end = tm.time()
-	#print("Uncompressed:", getS(), "Time:", end-start, "size", sz)
 	return end - start
 
 def writeCompressed():
 	start = tm.time()
 	settings['Compressed'] = 9
 	cdf_file = cdflib.cdfwrite.CDF(GLOBAL_FN, cdf_spec=settings,delete=True)
-	#####
 	globalAttrs={}
-	globalAttrs['extern_field']={0: gData["extern_field"]}
-	globalAttrs['datetime']={0: gData["datetime"]}
-	globalAttrs['arr_len']={0: gData["arr_len"]}
-	globalAttrs['lcr']={0: gData["lcr"]}
-	globalAttrs['ucr']={0: gData["ucr"]}
-	globalAttrs['ecr']={0: gData["ecr"]}
-	globalAttrs['geo_lat']={0: gData["geo_lat"]}
-	globalAttrs['geo_lon']={0: gData["geo_lon"]}
-	globalAttrs['geo_rad']={0: gData["geo_rad"]}
-	globalAttrs['loc_lat']={0: gData["loc_lat"]}
-	globalAttrs['loc_lon']={0: gData["loc_lon"]}
-	globalAttrs['starting_rig']={0: gData["starting_rig"]}
-	globalAttrs['rig_step']={0: gData["rig_step"]}
-	globalAttrs['step_limit']={0: gData["step_limit"]}
-	#######
+	for attrData in gData:
+		globalAttrs[attrData] = {0: gData[attrData]}
 	cdf_file.write_globalattrs(globalAttrs)
 	varinfo = {}
-	varinfo["Variable"] = "rig"
-	varinfo["Data_Type"] = cdflib.cdfwrite.CDF.CDF_REAL8 #'CDF_FLOAT'
+	varinfo["Data_Type"] = cdflib.cdfwrite.CDF.CDF_REAL8
 	varinfo["Num_Elements"] = 1
 	varinfo["Rec_Vary"] = 0
 	varinfo["Dim_Sizes"] = [sz]
 	varinfo["Compress"] = 9
-	cdf_file.write_var(varinfo, var_data=aData["rig"])
-	varinfo["Variable"] = "v"
-	cdf_file.write_var(varinfo, var_data=aData["v"] )
-	varinfo["Variable"] = "rad"
-	cdf_file.write_var(varinfo, var_data=aData["rad"] )
-	varinfo["Variable"] = "eth"
-	cdf_file.write_var(varinfo, var_data=aData["eth"])
-	varinfo["Variable"] = "efi"
-	cdf_file.write_var(varinfo, var_data=aData["efi"])
-	varinfo["Variable"] = "ath"
-	cdf_file.write_var(varinfo, var_data=aData["ath"])
-	varinfo["Variable"] = "afi"
-	cdf_file.write_var(varinfo, var_data=aData["afi"])
-	varinfo["Variable"] = "time"
-	cdf_file.write_var(varinfo, var_data=aData["time"])
-	varinfo["Variable"] = "length"
-	cdf_file.write_var(varinfo, var_data=aData["length"])
+	for attrData in aData:
+		varinfo["Variable"] = attrData
+		cdf_file.write_var(varinfo, var_data=aData[attrData])
+	
 	cdf_file.close()
 	end = tm.time()
-	#print("Compressed:", getS(), "Time:", end-start, "size", sz)
 	return end - start
 def read():
 	start = tm.time()
 	cdf_file = cdflib.cdfread.CDF(GLOBAL_FN)
 	attrs = cdf_file.globalattsget()
-	###
-	arrayData = {"rig":[],"v":[],"rad":[],"eth":[],"efi":[],"ath":[],"afi":[],"time":[],"length":[]}
-	globalData = {"arr_len": 0, "lcr": 0, "ucr": 0, "ecr": 0, "extern_field": None, "geo_lat": None,"geo_lon": None, "geo_rad": None, "loc_lat": None, "loc_lon": None, "datetime": None, "starting_rig": None, "rig_step": None, "step_limit": None}
-	##
-	globalData["arr_len"] = attrs['arr_len']
-	globalData["lcr"] = attrs['lcr']
-	globalData["ucr"] = attrs['ucr']
-	globalData["ecr"] = attrs['ecr']
-	globalData["extern_field"] = attrs['extern_field']
-	globalData["geo_lat"] = attrs['geo_lat']
-	globalData["geo_lon"] = attrs['geo_lon']
-	globalData["geo_rad"] = attrs['geo_rad']
-	globalData["loc_lat"] = attrs['loc_lat']
-	globalData["loc_lon"] = attrs['loc_lon']
-	globalData["datetime"] = attrs['datetime']
-	globalData["starting_rig"] = attrs['starting_rig']
-	globalData["rig_step"] = attrs['rig_step']
-	globalData["step_limit"] = attrs['step_limit']
-
-	arrayData["rig"] = cdf_file.varget("rig")
-	arrayData["v"] = cdf_file.varget("v")
-	arrayData["rad"] = cdf_file.varget("rad")
-	arrayData["eth"] = cdf_file.varget("eth")
-	arrayData["efi"] = cdf_file.varget("efi")
-	arrayData["ath"] = cdf_file.varget("ath")
-	arrayData["afi"] = cdf_file.varget("afi")
-	arrayData["time"] = cdf_file.varget("time")
-	arrayData["length"] = cdf_file.varget("length")
-	#print(arrayData["rig"])
+	arrData = {"rig":[],"v":[],"rad":[],"eth":[],"efi":[],"ath":[],"afi":[],"time":[],"length":[]}
+	globData = {"arr_len": 0, "lcr": 0, "ucr": 0, "ecr": 0, "extern_field": None, "geo_lat": None,"geo_lon": None, "geo_rad": None, "loc_lat": None, "loc_lon": None, "datetime": None, "starting_rig": None, "rig_step": None, "step_limit": None}
+	
+	for arrayData in arrData:
+		arrData[arrayData] = cdf_file.varget(arrayData)
+	
+	for attrData in globData:
+		globData[attrData] = attrs[attrData]
+	
 	cdf_file.close()
 	end = tm.time()
 	return end - start
@@ -161,8 +83,8 @@ cn.test(lambda: cn.extraCompressedRead(GLOBAL_FN, GLOBAL_ZIP_NAME), "zip compres
 cn.test(writeCompressed, "compressed w", GLOBAL_FN)
 cn.test(lambda: read(), "compressed r")
 
-cn.test(lambda: cn.extraCompression(GLOBAL_FN, GLOBAL_ZIP_NAME), "zip lossy compressed w", GLOBAL_ZIP_NAME)
-cn.test(lambda: cn.extraCompressedRead(GLOBAL_FN, GLOBAL_ZIP_NAME), "zip lossy compressed r")
+cn.test(lambda: cn.extraCompression(GLOBAL_FN, GLOBAL_ZIP_NAME), "zip compressed w", GLOBAL_ZIP_NAME)
+cn.test(lambda: cn.extraCompressedRead(GLOBAL_FN, GLOBAL_ZIP_NAME), "zip compressed r")
 
 
 #print(lat)
